8db56c8bf010e0fe52bb41c56fa03adcc7e48d5c
Acaki <axsz4298@gmail.com>
2020-04-10 15:16:03 +0800

    Add error displaying for login and register

--- src/Login.js
+++ src/Login.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Formik } from 'formik';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
 import { Redirect } from 'react-router-dom';
 import * as yup from 'yup';
 import { login } from './requests';
@@ -15,12 +16,22 @@ const schema = yup.object({
 const Login = () => {
   const { onLogin } = useAuthDataContext();
   const [redirectUri, setRedirectUri] = useState('');
-  const submitHandler = async (evt) => {
+  const submitHandler = async (evt, { setErrors }) => {
     const isValid = await schema.validate(evt);
     if (!isValid) {
       return;
     }
-    const response = await login(evt.username, evt.password);
+    let response;
+    try {
+      response = await login(evt.username, evt.password);
+    } catch (e) {
+      if (e.response.status === 401) {
+        setErrors(e.response.data);
+      } else {
+        setErrors({ other: e.response.data });
+      }
+      return;
+    }
     setRedirectUri(response.data.redirectUri);
     localStorage.setItem('loggedUser', JSON.stringify(response.data.loggedUser));
     onLogin(response.data.loggedUser);
@@ -40,17 +51,27 @@ const Login = () => {
         <Form onSubmit={formik.handleSubmit}>
           <Form.Group controlId="username">
             <Form.Label>Username</Form.Label>
-            <Form.Control type="text" {...formik.getFieldProps('username')} />
+            <Form.Control type="text" isInvalid={formik.errors.username} {...formik.getFieldProps('username')} />
+            <Form.Control.Feedback type="invalid">
+              {formik.errors.username}
+            </Form.Control.Feedback>
           </Form.Group>
 
           <Form.Group controlId="password">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" {...formik.getFieldProps('password')} />
+            <Form.Control type="password" isInvalid={formik.errors.password} {...formik.getFieldProps('password')} />
+            <Form.Control.Feedback type="invalid">
+              {formik.errors.password}
+            </Form.Control.Feedback>
           </Form.Group>
 
           <Button varians="primary" type="submit">
             Login
           </Button>
+
+          { formik.errors.other && (
+            <Alert variant="danger">{formik.errors.other}</Alert>
+          )}
         </Form>
       )}
     </Formik>
--- src/Register.js
+++ src/Register.js
@@ -6,6 +6,7 @@ import { Redirect } from 'react-router-dom';
 import * as yup from 'yup';
 import { register } from './requests';
 import { useAuthDataContext } from './AuthDataProvider';
+import Alert from 'react-bootstrap/Alert';
 
 const schema = yup.object({
   username: yup.string().required('Username is required'),
@@ -15,12 +16,22 @@ const schema = yup.object({
 const Register = () => {
   const { onLogin } = useAuthDataContext();
   const [redirectUri, setRedirectUri] = useState('');
-  const submitHandler = async (evt) => {
+  const submitHandler = async (evt, { setErrors }) => {
     const isValid = await schema.validate(evt);
     if (!isValid) {
       return;
     }
-    const response = await register(evt.username, evt.password);
+    let response;
+    try {
+      response = await register(evt.username, evt.password);
+    } catch (e) {
+      if (e.response.status === 401) {
+        setErrors(e.response.data);
+      } else {
+        setErrors({ other: e.response.data });
+      }
+      return;
+    }
     setRedirectUri(response.data.redirectUri);
     localStorage.setItem('loggedUser', JSON.stringify(response.data.loggedUser));
     onLogin(response.data.loggedUser);
@@ -40,7 +51,10 @@ const Register = () => {
         <Form onSubmit={formik.handleSubmit}>
           <Form.Group controlId="username">
             <Form.Label>Username</Form.Label>
-            <Form.Control type="text" {...formik.getFieldProps('username')} />
+            <Form.Control type="text" isInvalid={formik.errors.username} {...formik.getFieldProps('username')} />
+            <Form.Control.Feedback type="invalid">
+              {formik.errors.username}
+            </Form.Control.Feedback>
           </Form.Group>
 
           <Form.Group controlId="password">
@@ -51,6 +65,10 @@ const Register = () => {
           <Button varians="primary" type="submit">
             Register
           </Button>
+
+          { formik.errors.other && (
+            <Alert variant="danger">{formik.errors.other}</Alert>
+          )}
         </Form>
       )}
     </Formik>
