8866ea47d9973c206bcf088871f783c1359afd11
Acaki <axsz4298@gmail.com>
2020-04-10 16:29:44 +0800

    Add confirm for deleting user

--- src/tables/UserTable.js
+++ src/tables/UserTable.js
@@ -2,17 +2,27 @@ import React from 'react';
 import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
 
-const UserTable = (props) => (
-  <Table striped bordered hover>
-    <thead>
+const UserTable = (props) => {
+  const onDeleteUser = (userId) => {
+    return (e) => {
+      const confirm = window.confirm('Are you sure?');
+      if (confirm) {
+        props.removeUser(userId);
+      }
+    }
+  };
+
+  return (
+    <Table striped bordered hover>
+      <thead>
       <tr>
         <th>#</th>
         <th>name</th>
         <th>role</th>
         <th>actions</th>
       </tr>
-    </thead>
-    <tbody>
+      </thead>
+      <tbody>
       {props.users.map((user) => (
         <tr>
           <td>{user.id}</td>
@@ -20,12 +30,13 @@ const UserTable = (props) => (
           <td>{user.role}</td>
           <td>
             <Button type="button" onClick={() => props.editUser(user)}>Edit</Button>{' '}
-            <Button type="button" variant="danger" onClick={() => props.removeUser(user.id)}>Delete</Button>
+            <Button type="button" variant="danger" onClick={onDeleteUser(user.id)}>Delete</Button>
           </td>
         </tr>
       ))}
-    </tbody>
-  </Table>
-);
+      </tbody>
+    </Table>
+  )
+};
 
 export default UserTable;
